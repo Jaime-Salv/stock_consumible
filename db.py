5108c86df937261b96b7330edbd636334f7087b4
Jaime-Salv <jaime.rugomez@gmail.com>
2025-06-12 13:10:05 +0200

    Add files via upload

---
+++ db.py
@@ -0,0 +1,20 @@
+
+import os
+import psycopg2
+from urllib.parse import urlparse
+
+def get_connection():
+    database_url = os.environ.get("DATABASE_URL")
+    if not database_url:
+        raise ValueError("DATABASE_URL no está definida en las variables de entorno.")
+
+    result = urlparse(database_url)
+
+    conn = psycopg2.connect(
+        dbname=result.path[1:],
+        user=result.username,
+        password=result.password,
+        host=result.hostname,
+        port=result.port
+    )
+    return conn
