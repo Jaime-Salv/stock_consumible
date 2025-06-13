081795e670e6fd1dd159bc9970559b675b05f41b
Jaime-Salv <jaime.rugomez@gmail.com>
2025-06-13 16:38:31 +0200

    Update db.py

--- app/utils/db.py
+++ app/utils/db.py
@@ -3,7 +3,7 @@ import psycopg2
 import time
 from urllib.parse import urlparse
 
-def get_connection(retries=5, delay=3):
+def get_connection(retries=10, delay=5):
     database_url = os.getenv("DATABASE_URL")
     if not database_url:
         raise ValueError("DATABASE_URL no está definida.")
@@ -12,6 +12,7 @@ def get_connection(retries=5, delay=3):
 
     for attempt in range(1, retries + 1):
         try:
+            print(f"Intento {attempt}: conectando a la base de datos...")
             conn = psycopg2.connect(
                 dbname=result.path[1:],
                 user=result.username,
@@ -20,8 +21,12 @@ def get_connection(retries=5, delay=3):
                 port=result.port,
                 sslmode="require"
             )
+            print("✅ Conexión a la base de datos establecida.")
             return conn
         except psycopg2.OperationalError as e:
+            print(f"❌ Error de conexión ({attempt}/{retries}): {e}")
             if attempt == retries:
+                print("⛔ No se pudo establecer conexión con la base de datos después de varios intentos.")
                 raise
             time.sleep(delay)
+
