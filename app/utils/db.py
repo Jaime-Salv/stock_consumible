6eca8cca97a20aec749467b5be9773af3d5e4ccd
Jaime-Salv <jaime.rugomez@gmail.com>
2025-06-13 16:20:06 +0200

    Update db.py

--- app/utils/db.py
+++ app/utils/db.py
@@ -1,20 +1,27 @@
 import os
 import psycopg2
+import time
 from urllib.parse import urlparse
 
-def get_connection():
-    database_url = os.environ.get("DATABASE_URL")
+def get_connection(retries=5, delay=3):
+    database_url = os.getenv("DATABASE_URL")
     if not database_url:
-        raise ValueError("DATABASE_URL no está definida en las variables de entorno.")
+        raise ValueError("DATABASE_URL no está definida.")
 
     result = urlparse(database_url)
 
-    conn = psycopg2.connect(
-        dbname=result.path[1:],
-        user=result.username,
-        password=result.password,
-        host=result.hostname,
-        port=result.port,
-        sslmode="require"  # Muy importante para Render
-    )
-    return conn
+    for attempt in range(1, retries + 1):
+        try:
+            conn = psycopg2.connect(
+                dbname=result.path[1:],
+                user=result.username,
+                password=result.password,
+                host=result.hostname,
+                port=result.port,
+                sslmode="require"
+            )
+            return conn
+        except psycopg2.OperationalError as e:
+            if attempt == retries:
+                raise
+            time.sleep(delay)
